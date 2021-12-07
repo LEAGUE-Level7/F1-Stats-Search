5c41ca7d4001719eedee0bea297471c6f0dc1dbf
League Student <league@iMac-18.attlocal.net>
2021-12-06 17:58:36 -0800

    initial

---
+++ src/main/java/org/jointheleague/api/F1Stats/F1StatsApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.F1Stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1StatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1StatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jointheleague/api/F1Stats/F1StatsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.F1Stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1StatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
